06a5d5a39db329de55d11609150ce1ea456ec786
Chris Nystrom <chrisnys_88@hotmail.com>
2014-09-18 12:29:14 -0400

    Add Project Files before Live Server URL

---
+++ README.md
@@ -0,0 +1,21 @@
+# System Setup/Portfolio
+
+----
+## Live URL
+
+
+----
+## Description
+The purpose of this project is to set up a portfolio to present all the projects I'll complete in this course. I will also ensure that my Git and server workflow is in working order.
+
+----
+## Demo
+There is no demo requirement for this project(P1).
+
+----
+## Details for Teaching Team
+None.
+
+----
+## Outside code
+None.
---
+++ index.php
@@ -0,0 +1,82 @@
+
+<html>
+<head>
+
+    <title>Chris Nystrom P1 - DWA15 Portfolio</title>
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel='stylesheet' href='//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css' type='text/css'>
+    <link href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+
+        h1 {
+            text-align:center;
+        }
+
+        .container {
+            width:50%;
+            min-width:600px;
+            margin-bottom:50px;
+        }
+
+        img {
+            display:block;
+            margin:auto;
+            width:50%;
+        }
+
+    </style>
+
+
+</head>
+<body>
+
+
+<div class="container">
+
+    <h1>Chris Nystrom <br> Dynamic Web Apps Portfolio</h1>
+
+    <img src='images/cnystrom.jpg' alt='Chris Nystrom' height="300" width="42">
+
+    <h3>Project 2 Title</h3>
+    View: [link]
+    Github: [link]
+
+    <h3>Project 3 Title</h3>
+    View: [link]
+    Github: [link]
+
+    <h3>Project 4 Title</h3>
+    View: [link]
+    Github: [link]
+
+
+
+
+
+
+    <h2>In 2+ sentences describe who you are and what you do.</h2>
+    <p>Hello, my name is Chris Nystrom. I have 2 daughters. One in High School and one in Middle School. I work at IBM as a Technical Account Manager. My role requires me to work with Software Vendors that are
+        evaluating IBM Business Analytics software as an integrated part of their software solution.</p>
+
+    <h2>Why are you taking this course?</h2>
+    <p>I am taking this course in order to understand the process and software required to develop web applications. I am also interested in developing PHP skills and enhancing my HTML skills.</p>
+
+    <h2>What previous experience do you have with programming?</h2>
+    <p>I have taken several programming courses including Java, Javascript and C. I have also worked with Hadoop, Hive, Apache Pig and MapReduce</p>
+
+    <h2>Are you working on a Mac, PC, or some other platform?</h2>
+    <p>I am working on a PC running Windows 7.</p>
+
+    <h2>Anything else we should know?</h2>
+    <p>I love coaching soccer and enjoy watching/playing all sports!</p>
+
+</div>
+
+
+
+</body>
+</html>
